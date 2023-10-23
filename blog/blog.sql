cb544648aee25c97525f215a8c7b443662e89981
GyabiSito <121214489+GyabiSito@users.noreply.github.com>
2023-10-23 00:24:42 -0300

    Add files via upload

--- blog/blog.sql
+++ blog/blog.sql
@@ -1,28 +1,169 @@
-CREATE TABLE usuarios(
-id          int(255) auto_increment not null,
-nombre      varchar(100) not null,
-apellidos   varchar(100) not null,
-email       varchar(255) not null,
-password    varchar(255) not null,
-fecha       date not null,
-CONSTRAINT pk_usuarios PRIMARY KEY(id),
-CONSTRAINT uq_email UNIQUE(email)
-)ENGINE=InnoDb;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-10-2023 a las 05:23:24
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
-CREATE TABLE categorias(
-id      int(255) auto_increment not null,
-nombre  varchar(100),
-CONSTRAINT pk_categorias PRIMARY KEY(id)
-)ENGINE=InnoDb;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE entradas(
-id              int(255) auto_increment not null,
-usuario_id      int(255) not null,
-categoria_id    int(255) not null,
-titulo          varchar(255) not null,
-descripcion     MEDIUMTEXT,
-fecha           date not null,
-CONSTRAINT pk_entradas PRIMARY KEY(id),
-CONSTRAINT fk_entrada_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
-CONSTRAINT fk_entrada_categoria FOREIGN KEY(categoria_id) REFERENCES categorias(id) ON DELETE NO ACTION
-)ENGINE=InnoDb;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`) VALUES
+(1, 'Accion'),
+(2, 'Rol'),
+(3, 'Deportes'),
+(4, 'Plataformas'),
+(5, 'Sapo'),
+(6, 'Nuevos Juegos'),
+(7, 'Categoria');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entradas`
+--
+
+CREATE TABLE `entradas` (
+  `id` int(255) NOT NULL,
+  `usuario_id` int(255) NOT NULL,
+  `categoria_id` int(255) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` mediumtext DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `borrar` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `entradas`
+--
+
+INSERT INTO `entradas` (`id`, `usuario_id`, `categoria_id`, `titulo`, `descripcion`, `fecha`, `borrar`) VALUES
+(1, 14, 1, 'Novedades GTA', 'Descripcion GTA', '0000-00-00', 0),
+(2, 13, 1, 'Novedades GTA Vice City', 'Descripcion GTA', '2023-10-19', 0),
+(3, 13, 1, 'Novedades Lol', 'Descripcion Lol', '2023-10-19', 0),
+(6, 13, 1, 'Novedades Juego1', 'Descripcion Juego1', '2023-10-19', 0),
+(7, 13, 1, 'Novedades Juego2', 'Descripcion Juego2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ', '2023-10-19', 0),
+(8, 13, 4, 'Novedades Juego3', 'Descripcion Juego3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ', '2023-10-19', 0),
+(9, 13, 3, 'Novedades Juego4', 'Descripcion Juego4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ', '2023-10-19', 0),
+(10, 13, 2, 'Novedades Juego5', 'Descripcion Juego5Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ', '2023-10-19', 0),
+(11, 13, 1, 'Novedades Juego6', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ', '2023-10-19', 0),
+(12, 13, 2, 'sfdasdf', 'sfdgasdf', '2023-10-21', 0),
+(13, 13, 1, 'asdasd', 'asd', '2023-10-22', 1),
+(14, 13, 1, 'asdasd', 'asd', '2023-10-22', 1),
+(15, 13, 6, 'borrado1', 'borrado1', '2023-10-22', 1),
+(16, 13, 1, 'borrado1', 'asd', '2023-10-22', 1),
+(17, 13, 1, 'asdasdasd', 'asdasd', '2023-10-22', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `fecha`) VALUES
+(13, 'asd', 'asd', 'asd@gmail.com', '$2y$04$wqZ5zqtM3lIy2ssKhlGxDe5rdY4INwqMnEAvHtp58VUbLfpe5v1S.', '2023-10-19'),
+(14, 'asdf', 'asdf', 'asdf@gmail.com', '$2y$04$w4Ah6Np1E9BKTK8Rho7piOiGJzUif1PE0CrmzhnPv.0CuzutMeHTy', '2023-10-22');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_entrada_usuario` (`usuario_id`),
+  ADD KEY `fk_entrada_categoria` (`categoria_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD CONSTRAINT `fk_entrada_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `fk_entrada_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
